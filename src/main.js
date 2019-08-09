ab9e6c12fbfa443face3365e8e94f9b99a61a189
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2019-08-09 16:27:12 +0800

    搞定实时定位功能

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import { __ } from '@wordpress/i18n';
 import { withEffects, toProps } from 'refract-callbag';
 import { debounce } from 'lodash';
 import {
-	map,
-	pipe,
-	merge
+  map,
+  pipe,
+  merge
 } from 'callbag-basics';
 
 import { BaiduMap, Marker, NavigationControl } from 'react-baidu-maps';
@@ -47,27 +47,28 @@ class LocationPickerField extends Component {
   handleCityChange = (location) => {
     const { id, value, onChange, } = this.props;
 
-    let map = this.refs.BMap;
-    let geo = new BMap.Geocoder();
     let address = '北京市';
-    let el = this;
-    
-    if(location.province !== ''){
+
+    if (location.province !== '') {
       address = data['86'][location.province];
     }
 
-    if(location.city !== ''){
+    if (location.city !== '') {
       address = data['86'][location.province] + data[location.province][location.city];
     }
 
-    if(location.district !== ''){
+    if (location.district !== '') {
       address = data['86'][location.province] +
-              data[location.province][location.city] + 
-              data[location.city][location.district];
+        data[location.province][location.city] +
+        data[location.city][location.district];
     }
 
     // 解析地址为坐标
-    this.setAddress(address);
+    let map = this.refs.BMap;
+
+    if (address) {
+      this.props.onGeocodeAddress({ location, address, map });
+    }
 
     onChange(id, {
       ...value,
@@ -85,44 +86,22 @@ class LocationPickerField extends Component {
    * @param  {Object} e
    * @return {void}
    */
-  handleAddressChange = debounce( ( address ) => {
+  handleAddressChange = debounce((address) => {
     const { id, value, onChange, } = this.props;
 
-    this.setAddress(address);
+    let map = this.refs.BMap;
+    let location = address;
+
+    if (address) {
+      this.props.onGeocodeAddress({ location, address, map });
+    }
 
     onChange(id, {
       ...value,
       ...address
     });
 
-  }, 350 )
-  
-
-  /**
-   * 解析地址，修改中心点和 marker 位置
-   *
-   * @param  {Object} e
-   * @return {void}
-   */
-  setAddress = (address) => {
-    const { id, value, onChange, } = this.props;
-
-    let map = this.refs.BMap;
-    let geo = new BMap.Geocoder();
-    let el = this;
-
-    if(address !== ''){
-      geo.getPoint(address, function (point) {
-        if (point) {
-          map.centerAndZoom(point, 15);
-
-          el.props.value.lng = point.lng;
-          el.props.value.lat = point.lat;
-        }
-      }, data[value.province][value.city]);
-    }
-  
-  }
+  }, 350)
 
 
   /**
@@ -148,11 +127,11 @@ class LocationPickerField extends Component {
             onOptionChange={this.handleCityChange} />
 
           <SearchInput
-            id={ id }
+            id={id}
             className="rs-location__address"
-            name={ `${ name }[address]` }
-            value={ value.address }
-            onChange={ this.handleAddressChange }
+            name={`${name}[address]`}
+            value={value.address}
+            onChange={this.handleAddressChange}
           />
 
         </div>
@@ -218,4 +197,113 @@ class LocationPickerField extends Component {
   }
 }
 
-export default LocationPickerField;
+
+/**
+ * 控制副作用流
+ *
+ * @param  {Object} component
+ * @return {Object}
+ */
+const aperture = component => {
+  const [geocodeAddress$, geocodeAddress] = component.useEvent('geocodeAddress');
+
+  const geocodeAddressProps$ = pipe(
+    of({
+      onGeocodeAddress: geocodeAddress
+    }),
+    map(toProps)
+  );
+
+  const geocodeAddressEffect$ = pipe(
+    geocodeAddress$,
+    map((payload) => ({
+      type: 'GEOCODE_ADDRESS',
+      payload: payload
+    }))
+  );
+
+  return merge(geocodeAddressProps$, geocodeAddressEffect$);
+}
+
+
+/**
+ * The function that causes the side effects.
+ *
+ * @param  {Object} props
+ * @return {Function}
+ */
+const handler = props => effect => {
+  const { payload, type } = effect;
+  const {
+    id,
+    value,
+    onChange
+  } = props;
+
+  switch (type) {
+    case 'GEOCODE_ADDRESS':
+
+      // 解析地址的函数，解析成功会，通过 Resolve 返回参数
+      const geocode = (address) => {
+
+        return new Promise((resolve, reject) => {
+          let map = payload.map;
+          let location = payload.location;
+          let geo = new BMap.Geocoder();
+          let city = '';
+
+          console.log(location);
+          console.log(value);
+
+          if (location.city !== '') {
+            city = data[location.province][location.city];
+          }
+
+          if (city === ''){
+            console.log(222);
+            city = data[value.province][value.city];
+          }
+
+          console.log(city);
+
+          if (address !== '') {
+            geo.getPoint(address, (point) => {
+              if (point) {
+                map.centerAndZoom(point, 15);
+
+                resolve({
+                  lat: point.lat,
+                  lng: point.lng
+                });
+              } else {
+                console.log("Address decoder failed");
+              }
+
+            }, city);
+          }
+
+        });
+      };
+
+      // 地址解析后继续操作
+      geocode(payload.address)
+        .then(({ lat, lng }) => {
+          onChange(id, {
+            ...value,
+            address: payload.address,
+            lat: lat,
+            lng: lng
+          });
+
+          console.log(lat);
+        })
+        .catch((alert) => {
+          // eslint-disable-next-line
+          console.log(alert);
+        });
+
+      break;
+  }
+}
+
+export default withEffects(aperture, { handler })(LocationPickerField);
